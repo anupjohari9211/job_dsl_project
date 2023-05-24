3ef11b1c1f1cf3b717708976fda31cffde518473
anupjohari9211 <58782410+anupjohari9211@users.noreply.github.com>
2023-05-24 19:02:21 +0530

    Update single_script_multi_jobs.groovy
    new changes

--- single_script_multi_jobs.groovy
+++ single_script_multi_jobs.groovy
@@ -2,42 +2,41 @@ listView('My New View') {
     description('This is a new view')
     filterBuildQueue()
     filterExecutors()
+
     jobs {
-        regex('.Job*')  // Include all jobs in the view
-    }
-}
+        job('JobA') {
+            steps {
+                shell('echo "Running Job A"')
+                shell('echo "Step 2"')
+            }
+            publishers {
+                downstream('JobB')
+            }
+        }
 
-job('JobA') {
-    steps {
-        shell('echo "Running Job A"')
-        shell('echo "Step 2"')
-    }
-    publishers {
-        downstream('JobB')
-    }
-}
+        job('JobB') {
+            steps {
+                shell('echo "Running Job B"')
+                shell('echo "Step 1"')
+                shell('echo "Step 2"')
+            }
+            publishers {
+                downstream('JobC')
+            }
+        }
 
-job('JobB') {
-    steps {
-        shell('echo "Running Job B"')
-        shell('echo "Step 1"')
-        shell('echo "Step 2"')
-    }
-    publishers {
-        downstream('JobC')
-    }
-}
+        job('JobC') {
+            steps {
+                shell('echo "Running Job C"')
+                shell('echo "Step 1"')
+                shell('echo "Step 2"')
+                shell('echo "Step 3"')
+            }
+        }
 
-job('JobC') {
-    steps {
-        shell('echo "Running Job C"')
-        shell('echo "Step 1"')
-        shell('echo "Step 2"')
-        shell('echo "Step 3"')
+        regex('JobA|JobB|JobC')  // Include all jobs in the view
     }
-}
 
-listView('My New View') {
     columns {
         status()
         weather()
