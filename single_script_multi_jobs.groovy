f4a0350af68743f5e037da90d1efa1ece6fb5b08
anupjohari9211 <58782410+anupjohari9211@users.noreply.github.com>
2023-05-24 18:00:35 +0530

    Update single_script_multi_jobs.groovy
    updating the corrected script

--- single_script_multi_jobs.groovy
+++ single_script_multi_jobs.groovy
@@ -1,40 +1,41 @@
 listView('My New View') {
     description('This is a new view')
+
     filterBuildQueue()
     filterExecutors()
+
     jobs {
         job('JobA') {
             steps {
-                // Build steps for Job A
                 shell('echo "Running Job A"')
                 shell('echo "Step 2"')
-	    }
+            }
             publishers {
                 downstream('JobB')
-                }
             }
         }
+
         job('JobB') {
             steps {
-                // Build steps for Job B
                 shell('echo "Running Job B"')
                 shell('echo "Step 1"')
                 shell('echo "Step 2"')
             }
             publishers {
                 downstream('JobC')
-                }
             }
         }
+
         job('JobC') {
             steps {
-                // Build steps for Job C
                 shell('echo "Running Job C"')
                 shell('echo "Step 1"')
                 shell('echo "Step 2"')
                 shell('echo "Step 3"')
             }
         }
+    }
+
     columns {
         status()
         weather()
