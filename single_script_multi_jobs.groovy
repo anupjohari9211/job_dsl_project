f5b2751a02565a02c5cbb977471c839f14aa6c2d
anupjohari9211 <58782410+anupjohari9211@users.noreply.github.com>
2023-05-24 18:24:44 +0530

    Update single_script_multi_jobs.groovy
    updating scripts with job names

--- single_script_multi_jobs.groovy
+++ single_script_multi_jobs.groovy
@@ -2,39 +2,40 @@ listView('My New View') {
     description('This is a new view')
     filterBuildQueue()
     filterExecutors()
+    jobNames('JobA', 'JobB', 'JobC')
+}
 
-    jobs {
-        job('JobA') {
-            steps {
-                shell('echo "Running Job A"')
-                shell('echo "Step 2"')
-            }
-            publishers {
-                downstream('JobB')
-            }
-        }
-
-        job('JobB') {
-            steps {
-                shell('echo "Running Job B"')
-                shell('echo "Step 1"')
-                shell('echo "Step 2"')
-            }
-            publishers {
-                downstream('JobC')
-            }
-        }
-
-        job('JobC') {
-            steps {
-                shell('echo "Running Job C"')
-                shell('echo "Step 1"')
-                shell('echo "Step 2"')
-                shell('echo "Step 3"')
-            }
-        }
+job('JobA') {
+    steps {
+        shell('echo "Running Job A"')
+        shell('echo "Step 2"')
     }
+    publishers {
+        downstream('JobB')
+    }
+}
 
+job('JobB') {
+    steps {
+        shell('echo "Running Job B"')
+        shell('echo "Step 1"')
+        shell('echo "Step 2"')
+    }
+    publishers {
+        downstream('JobC')
+    }
+}
+
+job('JobC') {
+    steps {
+        shell('echo "Running Job C"')
+        shell('echo "Step 1"')
+        shell('echo "Step 2"')
+        shell('echo "Step 3"')
+    }
+}
+
+listView('My New View') {
     columns {
         status()
         weather()
