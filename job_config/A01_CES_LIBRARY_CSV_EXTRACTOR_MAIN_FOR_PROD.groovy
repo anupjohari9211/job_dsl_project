f25c78ca6cf86db387ab4e9acfb73d27f79c62f1
anupjohari9211 <58782410+anupjohari9211@users.noreply.github.com>
2023-05-26 17:01:40 +0530

    Update A01_CES_LIBRARY_CSV_EXTRACTOR_MAIN_FOR_PROD.groovy
    commit

--- job_config/A01_CES_LIBRARY_CSV_EXTRACTOR_MAIN_FOR_PROD.groovy
+++ job_config/A01_CES_LIBRARY_CSV_EXTRACTOR_MAIN_FOR_PROD.groovy
@@ -1,46 +1,48 @@
-import hudson.model.*
-import hudson.model.ListView.*
-
-def jobName = 'A1. CES_LIBRARY_CSV_EXTRACTOR_MAIN_FOR_PROD'
-def viewName = 'content-studio CC Flow'
-
-// Create a new view
-def listView = new ListView(viewName, Hudson.getInstance())
-listView.description = 'This is a new CC view'
-listView.filterQueue = true
-listView.filterExecutors = true
-
-// Specify the columns for the view
-listView.columns = [
-    new StatusColumn(),
-    new WeatherColumn(),
-    new JobColumn(),
-    new LastSuccessColumn(),
-    new LastFailureColumn(),
-    new DurationColumn()
-]
-
-// Create the multi-configuration job
-def job = new FreeStyleProject(Hudson.getInstance(), jobName)
-job.description = 'Load the Course details from CES database for given production portal'
-
-// Discard Old Builds
-job.addProperty(new hudson.model.BuildDiscarderProperty(new hudson.tasks.LogRotator(numToKeepStr: '4', artifactNumToKeepStr: '4')))
-
-// Source Code Management
-job.setScm(new hudson.scm.NullSCM())
-
-// Build Triggers
-job.addTrigger(new hudson.triggers.TimerTrigger('H 02 * * 1-5'))
-
-// Build Environment
-job.getBuildWrappersList().add(new hudson.tasks.MasqueradePasswordsBuildWrapper())
-
-// Post-Build Actions
-job.getPublishersList().add(new hudson.tasks.BuildTrigger('A11. CONTENT_STUDIO_SUBSCRIPTION_EXTRACTOR_MAIN', Result.SUCCESS))
-
-// Add the job to the view
-listView.add(job)
-
-// Save the view
-Hudson.getInstance().addView(listView)
+listView('CC Flow') {
+    description('This is a new CC view')
+    filterBuildQueue()
+    filterExecutors()
+  
+    matrixProject('A1. CES_LIBRARY_CSV_EXTRACTOR_MAIN_FOR_PROD') {
+        description('Load the Course details from CES database for given production portal')
+  
+        // Discard Old Builds
+        configure { project ->
+            project / 'properties' << 'jenkins.model.BuildDiscarderProperty' {
+                strategy {
+                    numToKeepStr('4')
+                    artifactNumToKeepStr('4')
+                }
+            }
+        }
+  
+        // Source Code Management
+        scm {
+            none()
+        }
+  
+        // Build Triggers
+        triggers {
+            cron('H 02 * * 1-5')
+        }
+  
+        // Build Environment
+        wrappers {
+            maskPasswords()
+        }
+  
+        // Post-Build Actions
+        publishers {
+            downstream('A11. CONTENT_STUDIO_SUBSCRIPTION_EXTRACTOR_MAIN')
+        }
+    }
+  
+    columns {
+        status()
+        weather()
+        name()
+        lastSuccess()
+        lastFailure()
+        lastDuration()
+    }
+}
