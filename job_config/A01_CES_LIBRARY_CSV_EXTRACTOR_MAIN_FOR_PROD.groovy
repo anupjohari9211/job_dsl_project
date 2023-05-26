a6d8cf829d3c01501a206430e1e2b2c96a6d4ff3
anupjohari9211 <58782410+anupjohari9211@users.noreply.github.com>
2023-05-26 17:12:58 +0530

    Update A01_CES_LIBRARY_CSV_EXTRACTOR_MAIN_FOR_PROD.groovy
    new script

--- job_config/A01_CES_LIBRARY_CSV_EXTRACTOR_MAIN_FOR_PROD.groovy
+++ job_config/A01_CES_LIBRARY_CSV_EXTRACTOR_MAIN_FOR_PROD.groovy
@@ -1,3 +1,28 @@
-multijob('Multi-Configuration Job') {
-    description('This is a multi-configuration job')
+listView('content studio cc flow') {
+    description('This is a new flow')
+    filterBuildQueue()
+    filterExecutors()
+
+    jobs {
+        job('A01. CES_LIBRARY_CSV_EXTRACTOR_MAIN_FOR_PROD') {
+            steps {
+                shell('echo "Running Job A"')
+                shell('echo "Step 2"')
+            }
+            publishers {
+                downstream('JobB')
+            }
+        }
+
+        regex('A01. CES_LIBRARY_CSV_EXTRACTOR_MAIN_FOR_PROD')  // Include all jobs in the view
+    }
+
+    columns {
+        status()
+        weather()
+        name()
+        lastSuccess()
+        lastFailure()
+        lastDuration()
+    }
 }
