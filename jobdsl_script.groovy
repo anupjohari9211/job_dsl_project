06242b88f8cc512c421aa25d56944b86e3d69795
anupjohari9211 <58782410+anupjohari9211@users.noreply.github.com>
2023-05-26 17:15:46 +0530

    Update jobdsl_script.groovy
    new

--- jobdsl_script.groovy
+++ jobdsl_script.groovy
@@ -6,21 +6,3 @@ def loadScriptFromSCM(String scmPath) {
 // Load the job creation configuration
 def jobConfig = loadScriptFromSCM('job_config/A01_CES_LIBRARY_CSV_EXTRACTOR_MAIN_FOR_PROD.groovy')
 
-// Load the axes configuration
-def axesConfig = loadScriptFromSCM('axes/axes_config.groovy')
-
-// Load the build steps
-def buildSteps = loadScriptFromSCM('build_steps/build_steps.groovy')
-
-// Create the multi-configuration job
-jobConfig.call()
-
-// Modify the multi-configuration job with axes and build steps
-matrixProject('Multi-Job Configuration') {
-    axes {
-        axesConfig.call()
-    }
-    steps {
-        buildSteps.call()
-    }
-}
